4a15886ae6db8b26430714e9c7bc55ec3d8c033e
Simon Peterhans <simon.peterhans@gmail.com>
2021-06-30 17:07:10 +0200

    Update build script and basic project structure.

---
+++ src/main/kotlin/ch/unibas/dmi/dbis/som/Main.kt
@@ -0,0 +1,9 @@
+package ch.unibas.dmi.dbis.som
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main() {
+    logger.info("Hello World")
+}
--- src/main/kotlin/som/kt/Main.kt
+++
@@ -1,5 +0,0 @@
-package som.kt
-
-fun main() {
-
-}
